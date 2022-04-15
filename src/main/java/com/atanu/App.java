fb9798a0f10515784e3eb7ba3be80514db2d3610
atanu <atanuj383@gmail.com>
2022-04-15 12:22:37 +0530

    first commit

---
+++ src/main/java/com/atanu/App.java
@@ -0,0 +1,15 @@
+package com.atanu;
+
+/**
+ * Hello world!
+ * //java -cp target/hellomaven-1.0-SNAPSHOT.jar com.atanu.App
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    { 
+        System.out.println(5+3+2);
+    }
+}
